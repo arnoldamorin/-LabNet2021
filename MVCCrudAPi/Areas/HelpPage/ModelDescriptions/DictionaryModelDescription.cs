8d878907bfdcecc10de3f33d9d7ea82cbabde244
Arnold Amorin <aamorin.ar@gmail.com>
2021-05-10 16:32:53 -0300

    api

---
+++ MVCCrudAPi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MVCCrudAPi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MVCCrudAPi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCCrudAPi.WebApiApplication" Language="C#" %>
